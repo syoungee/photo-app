4925d7b366f1ed6fd19190e0b9596b64ce0fa0bc
syoungee <liahwang@tecace.com>
2024-01-09 13:28:17 +0900

    feature / router 설정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import { Outlet } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      App.js
-    </div>
+    <>
+      <Outlet />
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NotFound from './components/NotFound';
+import MainPage from './pages/MainPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <MainPage></MainPage>,
+      },
+      {
+        path: '',
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
