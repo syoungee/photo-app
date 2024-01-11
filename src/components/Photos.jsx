cff6dd084ac15fcfa3749143d030368d316f5b1d
syoungee <sunyoungah69@gmail.com>
2024-01-11 14:21:12 +0900

    feature / 좋아요, 좋아요 취소 기능 구현

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -1,31 +1,60 @@
 import React, { useEffect, useState } from 'react';
 import unsplashAxios from '../services/unsplashAxios';
+import { FaRegHeart } from 'react-icons/fa';
+import { FaHeart } from 'react-icons/fa';
 
 export default function Photos() {
   const [photos, setPhotos] = useState(null);
 
-  const likePhoto = (unsplashId) => {
+  const likePhoto = async (unsplashId) => {
     try {
       console.log('photo id?', unsplashId);
-      unsplashAxios.post(`photos/${unsplashId}/like`).then((res) => {
-        console.log(res);
-      });
+      await unsplashAxios.post(`photos/${unsplashId}/like`);
+      setPhotos((prevPhotos) =>
+        prevPhotos.map((photo) => {
+          if (photo.id === unsplashId) return { ...photo, liked_by_user: !photo.liked_by_user };
+          else return photo;
+        })
+      );
     } catch (error) {
       console.log('Error fetching like data:', error);
     }
   };
 
-  useEffect(() => {
-    const fetchRandomPhoto = async () => {
-      try {
-        const response = await unsplashAxios.get('photos');
-        console.log('response', response);
-        setPhotos(response?.data);
-      } catch (error) {
-        console.error('Error fetching random photo:', error);
-      }
-    };
+  const unlikePhoto = async (unsplashId) => {
+    try {
+      console.log('photo id?', unsplashId);
+      await unsplashAxios.delete(`photos/${unsplashId}/like`);
+      setPhotos((prevPhotos) =>
+        prevPhotos.map((photo) => {
+          if (photo.id === unsplashId) return { ...photo, liked_by_user: !photo.liked_by_user };
+          else return photo;
+        })
+      );
+    } catch (error) {
+      console.log('Error fetching like data:', error);
+    }
+  };
 
+  const handleClick = (flag, unsplashId) => {
+    if (flag) {
+      unlikePhoto(unsplashId);
+    } else {
+      likePhoto(unsplashId);
+    }
+  };
+
+  const fetchRandomPhoto = async () => {
+    try {
+      const response = await unsplashAxios.get('photos');
+      console.log('response', response);
+      setPhotos(response?.data);
+    } catch (error) {
+      console.error('Error fetching random photo:', error);
+    }
+  };
+
+  useEffect(() => {
     fetchRandomPhoto();
   }, []);
 
@@ -35,16 +64,12 @@ export default function Photos() {
       <div className="grid grid-cols-1 gap-x-4 gap-y-4 p-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4">
         {photos &&
           photos.map((photo, index) => (
-            <div key={index} className="items-center mx-auto">
+            <div key={index} className="items-center mx-auto relative">
               <img className="object-cover w-80 h-80 rounded-md" src={photo?.urls?.regular} alt={photo?.alt_description} />
               {/* <p>{photo?.description}</p> */}
-              <button
-                onClick={() => likePhoto(photo.id)}
-                className={`z-10 relative bottom-10 left-20 text-xl ${photo?.liked_by_user ? 'text-red-500' : 'text-gray-500'}`}
-              >
+              <button onClick={() => handleClick(photo?.liked_by_user, photo.id)} className={`absolute bottom-5 right-5 z-10 text-xl`}>
                 {/* 좋아요 아이콘 */}
-                버튼 여기!
-                {photo?.liked_by_user ? '❤️' : '⭐️'}
+                {photo?.liked_by_user ? <FaHeart color="#Ec5642" style={{ stroke: '#F2F4F6', strokeWidth: '30' }} /> : <FaRegHeart color="#F2F4F6" />}
               </button>
             </div>
           ))}
