d4d15bb7b5dcd43b2c7331a44582c5e7fe96c027
syoungee <sunyoungah69@gmail.com>
2024-01-12 14:11:00 +0900

    feature / header에 navigation 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import Logo from './Logo';
 import { BiHeart } from 'react-icons/bi';
+import { useNavigate } from 'react-router-dom';
 
-export default function header() {
+export default function Header() {
+  const navigate = useNavigate();
+
+  const toMainPage = () => {
+    console.log('to main page');
+    navigate('/');
+  };
+
+  const toBookmarkPage = () => {
+    console.log('to book mark page');
+    navigate('/bookmark');
+  };
+
   return (
     <div className="flex items-center w-full p-4  border-b ">
-      <Logo />
+      <div onClick={toMainPage} className="cursor-pointer">
+        <Logo />
+      </div>
       <div className="flex ml-auto items-center">
         <button className="border border-gray-200 text-gray-500 bg-gray-200 hover:bg-gray-300 font-bold py-1 px-2 rounded mr-2">사진 제출</button>
-        <button className="flex items-center border border-gray-200 text-gray-500 border-gray-200 hover:bg-gray-300 font-bold py-1 px-2 rounded mr-2">
+        <button
+          className="flex items-center border border-gray-200 text-gray-500 border-gray-200 hover:bg-gray-300 font-bold py-1 px-2 rounded mr-2"
+          onClick={toBookmarkPage}
+        >
           북마크 <BiHeart className="ml-0.5" />
         </button>
         <p className="text-gray-500 font-bold">Sunyoung | sunyoung@willog.io</p>
